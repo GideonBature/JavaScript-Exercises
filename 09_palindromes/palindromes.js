fb531462982ae078a0cc34ca29f96ebb8a84b9f0
GideonBature <infoaboutgideon@gmail.com>
2022-11-01 21:18:54 +0100

    completed palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
 
+    let exp = /[a-z]/gi;
+
+    let mainStr = str.match(exp).join('').toLowerCase();
+    let reverseStr = str.match(exp).reverse().join('').toLowerCase();
+    
+    return mainStr === reverseStr;
 };
 
 // Do not edit below this line
