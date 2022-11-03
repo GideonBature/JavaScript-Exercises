8818399ebdd183072659b5c3eba53047f272cec6
GideonBature <infoaboutgideon@gmail.com>
2022-11-03 02:44:28 +0100

    complete the find the oldest task

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(items) {
+    let title1 = items[0].title;
+    let title2 = items[1].title;
+    let titleArray = [title1, title2];
+    return titleArray;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,35 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let firstP = people[0];
+    let secondP = people[1];
+    let thirdP = people[2];
+    let date = new Date();
+    let year = date.getFullYear();
 
+    
+    if(!people[0].yearOfDeath) {
+        let firstPAge = year - firstP.yearOfBirth;
+        let secondPAge = secondP.yearOfDeath - secondP.yearOfBirth;
+        let thirdPAge = thirdP.yearOfDeath - thirdP.yearOfBirth;
+        if ((firstPAge > secondPAge) && (firstPAge > thirdPAge)) {
+            return firstP;
+        } else if ((secondPAge > firstPAge) && (secondPAge > thirdPAge)) {
+            return secondP;
+        } else {
+        return thirdP;
+        }
+    } else {
+        let firstPAge = firstP.yearOfDeath - firstP.yearOfBirth;
+        let secondPAge = secondP.yearOfDeath - secondP.yearOfBirth;
+        let thirdPAge = thirdP.yearOfDeath - thirdP.yearOfBirth;
+        if ((firstPAge > secondPAge) && (firstPAge > thirdPAge)) {
+            return firstP;
+        } else if ((secondPAge > firstPAge) && (secondPAge > thirdPAge)) {
+            return secondP;
+        } else {
+        return thirdP;
+        }
+    }
+    
 };
 
 // Do not edit below this line
